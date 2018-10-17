9c48da243ac877b0d86855d0bff862f8b082a87d
赵传昊 <724356121@qq.com>
2018-10-17 23:09:07 +0800

    Initial commit

---
+++ src/com/hd/dto/Main.java
@@ -0,0 +1,23 @@
+package com.hd.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        //测试一下写的这几个方法
+        MyArray<Integer> myArray = new MyArray();
+        myArray.add(1);
+        myArray.add(2);
+        myArray.add(0, 0);
+        System.out.println(myArray);
+        myArray.set(0, -1);
+        System.out.println(myArray.get(0));
+        myArray.remove(0);
+        System.out.println(myArray);
+
+
+    }
+}
